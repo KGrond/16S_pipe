ec2d4ab23b0c44fea4866249bd44f8e81c6bf914
Kirsten Grond <109685205+KGrond@users.noreply.github.com>
2023-06-23 08:56:18 -0800

    Update QIIME2_core.sh

--- QIIME2_core.sh
+++ QIIME2_core.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-export PWD='/Users/kirstengrond/Desktop/Snapi'
+export PWD='/Users/kirstengrond/Desktop'
 export CURDIR=${PWD}
 export RESULTS=${SEQ}/results
 export MPLCONFIGDIR=${CURDIR}/tmp
@@ -11,5 +11,5 @@ export SEQ=${CURDIR}/sequences
 ### loop through qiime2 script for every directory.
 
 for dir in ${SEQ}/* ; do
-	cd $dir && sh /Users/kirstengrond/Desktop/Snapi/Scripts/qiime2.sh ;
+	cd $dir && sh /Users/kirstengrond/Desktop/Scripts/qiime2.sh ;
 done
